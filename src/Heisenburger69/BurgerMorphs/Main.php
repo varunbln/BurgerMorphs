16bac3c1d6322f5a361e84e1c73dfad69c7db1b1
Heisenburger69 <apkforgss@gmail.com>
2021-07-26 19:27:08 +0530

    init

---
+++ src/Heisenburger69/BurgerMorphs/Main.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Heisenburger69\BurgerMorphs;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase
+{
+    public function onEnable()
+    {
+
+    }
+}
